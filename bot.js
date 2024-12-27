b9d837a6c842943889530d161e57e907f9817c20
KelvinTrinhSG <126239251+KelvinTrinhSG@users.noreply.github.com>
2024-12-27 20:26:53 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ bot.onText(/\/start/, (msg) => {
                      // Văn bản trên nút
                     // URL web app khi nhấn nút
                     // Thay đổi URL sang trang web mới
-                    { text: 'Play opBNB Flash Ball Telegram Game', web_app: { url: 'https://itch.io/embed-upload/12348200?color=333333' } }
+                    { text: 'Play opBNB Disk Frenzy Telegram Game', web_app: { url: 'https://itch.io/embed-upload/12348255?color=333333' } }
                 ]
             ]
         }
